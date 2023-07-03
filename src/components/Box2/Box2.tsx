e2a1cd39caa3f7f150ed7f8d200456bb798689cb
tahawy111 <amertahawy111@gmail.com>
2023-07-03 18:11:46 +0300

    .

--- src/components/Box2/Box2.tsx
+++ src/components/Box2/Box2.tsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 
 interface Box2Props {}
 
 export default function Box2({}: Box2Props) {
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
+  const dragContainer = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="pb-40">
+    <div ref={dragContainer} className="pb-40">
       <motion.div
         className="w-64 h-64 bg-gradient-to-tr to-blue-600 from-teal-600 flex flex-col items-center justify-center"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         drag
+        dragConstraints={{
+          right: 20,
+          left: -20,
+          top: 5,
+          bottom: 5,
+        }}
       ></motion.div>
     </div>
   );
